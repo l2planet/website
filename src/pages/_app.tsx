2ce5b2c4b04db1dbbd17d6b9549e0b38195c60a8
Berzan <berzan@tutanota.com>
2022-09-26 14:19:15 +0300

    update: _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,24 @@
 import '../styles/globals.css'
+import '../styles/font.css'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from '../contexts/ThemeContext'
+import { TopBar } from '../components/TopBar'
+import { Main } from '../components/Main'
+import { Footer } from '../components/Footer'
+import { ApiProvider } from '../contexts/ApiContext'
         
+// This component is shared by every page.
+// The data on it is stored across different pages.
 function MyApp({ Component, pageProps }: AppProps) {
     return (
         <ThemeProvider>
-            <Component {...pageProps} />
+            <ApiProvider>
+                <TopBar/>
+                <Main>
+                    <Component {...pageProps} />
+                </Main>
+                <Footer/>
+            </ApiProvider>
         </ThemeProvider>
     )
 }
