52cd26dae2992071b7f8d14c2760a3cd7eb185c7
Berzan <berzan@tutanota.com>
2022-09-26 14:17:26 +0300

    create: SEO.tsx

---
+++ src/components/SEO.tsx
@@ -0,0 +1,19 @@
+import Head from "next/head"
+
+type SEOComponent = (props: SEOProps) => JSX.Element
+
+interface SEOProps {
+    title: string
+    description: string
+    favicon: string
+}
+
+
+
+export const SEO: SEOComponent = (props: SEOProps) => (
+    <Head>
+        <title>{props.title}</title>
+        <meta name="description" content={props.description}/>
+        <link rel='icon' href={props.favicon}/>
+    </Head>
+)
