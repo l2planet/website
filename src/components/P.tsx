f93153d3ecb037375829937b4031781365be23fa
Berzan <berzan@tutanota.com>
2022-09-26 14:16:51 +0300

    create: P.tsx

---
+++ src/components/P.tsx
@@ -0,0 +1,52 @@
+import { wrapn } from "wrapn";
+
+export const P1 = wrapn('p')`
+    text-center
+
+    font-semibold
+
+    leading-normal
+
+    text-sm
+    sm:text-base
+    md:text-lg
+    lg:text-xl
+
+    text-gris-7
+    dark:text-gris-2
+`
+
+
+export const PChainDescription = wrapn('p')`
+    font-semibold
+
+    leading-normal
+
+    text-sm
+    sm:text-base
+    md:text-lg
+    lg:text-xl
+
+    text-gris-7
+    dark:text-gris-2
+`
+
+
+export const PLayer2Description = wrapn('p')`
+    break-words
+
+    font-semibold
+
+    leading-normal
+
+    text-sm
+    sm:text-base
+    md:text-lg
+
+    text-gris-7
+    dark:text-gris-2
+`
+
+ 
+
+
