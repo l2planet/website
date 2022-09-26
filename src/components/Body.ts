e29b3ac82b169dbca26df00875cf3d788d6c5de5
Berzan <berzan@tutanota.com>
2022-09-26 14:12:39 +0300

    create: Body.ts

---
+++ src/components/Body.ts
@@ -0,0 +1,13 @@
+import { wrapn } from 'wrapn'
+
+/** Wrapped `<body/>` component. */
+export const Body = wrapn('body')`
+    font-medium
+    leading-tight
+
+    text-gris-9
+    dark:text-gris-0
+    
+    bg-gris-0
+    dark:bg-gris-9
+`
