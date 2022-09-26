5e97d3fe9b7792a8d5e25f3d375d3a16b634d7ae
Berzan <berzan@tutanota.com>
2022-09-26 14:16:20 +0300

    create: Main.tsx

---
+++ src/components/Main.tsx
@@ -0,0 +1,27 @@
+import { wrapn } from 'wrapn'
+
+export const Main = wrapn('main')`
+    flex
+    flex-col
+
+    self-center
+
+    flex-1
+    w-full
+    max-w-6xl
+    
+    px-4
+    sm:px-6
+    md:px-8
+    lg:px-10
+
+    py-2
+    sm:py-3
+    md:py-4
+    lg:py-5
+
+    space-y-10
+    sm:space-y-14
+    md:space-y-18
+    lg:space-y-20
+`
