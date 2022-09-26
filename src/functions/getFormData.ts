f5baeceb41644370ed0436da6932253a8e837784
Berzan <berzan@tutanota.com>
2022-09-26 14:10:32 +0300

    create: getFormData.ts

---
+++ src/functions/getFormData.ts
@@ -0,0 +1,8 @@
+import { FormEvent } from "react";
+
+/**
+ * Generic function that extracts form data from a `FormEvent`.
+ */
+export function getFormData<T>(event: FormEvent<HTMLFormElement>) {
+    return Object.fromEntries(new FormData(event.currentTarget)) as T
+}
