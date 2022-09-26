6069b2d2389e1cd49d40d53d04c305ec8c545c08
Berzan <berzan@tutanota.com>
2022-09-26 14:05:18 +0300

    update: globals.ts

--- src/types/globals.ts
+++ src/types/globals.ts
@@ -1,5 +1,26 @@
-import { ReactNode } from "react";
+import { FormEventHandler, ReactNode } from "react";
+import { RawFormChain, RawFormLayer2, RawFormLogin, RawFormProject } from "./Api";
 
-export interface FCWithChildren {
+export interface ChildrenProp {
     children: ReactNode
+}
+
+
+export type RawFormData = RawFormChain | RawFormLayer2 | RawFormProject | RawFormLogin
+
+export interface FormProps<T extends RawFormData> {
+    onSubmit: (formData: T) => void
+}
+
+export interface LabeledInputProps {
+    label: string
+    name: string
+    tip: string
+    isPassword?: boolean 
+}
+
+
+export interface ChartDataItem {
+    t: string,
+    v?: number,
 }
