0d0aa4faac900e2980901aedbc4aad11fb575fef
Berzan <berzan@tutanota.com>
2022-09-26 14:15:09 +0300

    create: icon components

---
+++ src/components/icons/IconStats.tsx
@@ -0,0 +1,5 @@
+export const IconStats = () => (
+    <svg xmlns='http://www.w3.org/2000/svg' className='fill-gris-9 dark:fill-gris-0' viewBox='0 0 48 48' height='100%' width='100%'>
+        <path d="M8 40V18h7v22Zm12.5 0V8h7v32ZM33 40V26h7v14Z"/> 
+    </svg>
+)
