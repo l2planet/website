d3de1e2e871864f5c16a5e9627ff3a13ea7f08af
Berzan <berzan@tutanota.com>
2022-09-26 14:21:43 +0300

    create: newsletter page

---
+++ src/pages/newsletter.tsx
@@ -0,0 +1,21 @@
+/* eslint-disable react/no-unescaped-entities */
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import { SEO } from '../components/SEO'
+
+const Newsletter: NextPage = () => {
+    return(
+        <>
+            <SEO
+                title='L2 Planet | Newsletter'
+                description='L2 Planet'
+                favicon='/favicon.ico'
+            />
+            
+        </>
+    )
+}
+            
+export default Newsletter
+            
