289d12863389efb87383bd70d79de2afbe207548
Berzan <berzan@tutanota.com>
2022-09-26 14:21:30 +0300

    create: panel page

---
+++ src/pages/members/panel.tsx
@@ -0,0 +1,32 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import { APanel } from '../../components/A'
+import { H1 } from '../../components/H'
+import { Link } from '../../components/Link'
+import { SectionPanel } from '../../components/Section'
+import { SEO } from '../../components/SEO'
+
+const Panel: NextPage = () => {
+    return(
+        <>
+            <SEO
+                title='L2 Planet | Panel'
+                description='L2 Planet'
+                favicon='/favicon.ico'
+            />
+
+            <H1>Admin Panel</H1>
+            
+            <SectionPanel>
+                <Link a={APanel} href='/members/new/chain'>Chain</Link>
+                <Link a={APanel} href='/members/new/layer2'>Layer2</Link>
+                <Link a={APanel} href='/members/new/project'>Project</Link>
+                <Link a={APanel} href='/members/new/newsletter'>Newsletter</Link>
+            </SectionPanel>
+        </>
+    )
+}
+            
+export default Panel
+            
