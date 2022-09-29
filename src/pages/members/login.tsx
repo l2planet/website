b66d762b88c204f3b304a9b39e5da0ca7e9ef29a
Berzan <berzan@tutanota.com>
2022-09-29 10:37:17 +0300

    update: onSubmit event

--- src/pages/members/login.tsx
+++ src/pages/members/login.tsx
@@ -1,19 +1,11 @@
 /* eslint-disable react/no-unescaped-entities */
-import { wrap } from 'module'
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import { useEffect, useState } from 'react'
 import { wrapn } from 'wrapn'
-import { ButtonForm } from '../../components/Button'
 import { LoginForm } from '../../components/Form'
 import { H1 } from '../../components/H'
 import { Link } from '../../components/Link'
 import { SEO } from '../../components/SEO'
-import { authLogin } from '../../functions/auth'
-import { getCookie, setCookie } from '../../functions/cookie'
-import { getFormData } from '../../functions/getFormData'
-import { makePostRequest } from '../../functions/makeRequest'
+import { authLogin } from '../../functions/api'
 
 const Members: NextPage = () => {
 
@@ -28,17 +20,13 @@ const Members: NextPage = () => {
             <H1>Login</H1>
             
             <Div>
-                <LoginForm onSubmit={async (data) => {
-                    const res = await authLogin(data);
-                    
-                    if(res === null) {
-                        alert('Wrong password or username.')
-                    } else {
+                <LoginForm onSubmit={async (formData) => {
+                    try {
+                        await authLogin(formData)
                         alert('Succesfully logged in!')
-                        console.log(res)
-                        setCookie('jwt', res.token, res.expire)
+                    } catch (e: any) {
+                        alert(e.message)
                     }
-
                 }}/>
 
                 <Link a={A} href='/members/register'>Don't have an account? Register</Link>
--- src/pages/members/register.tsx
+++ src/pages/members/register.tsx
@@ -1,24 +1,17 @@
 /* eslint-disable react/no-unescaped-entities */
-import { wrap } from 'module'
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
 import { useRouter } from 'next/router'
-import { useState } from 'react'
 import { wrapn } from 'wrapn'
-import { ButtonForm } from '../../components/Button'
 import { LoginForm } from '../../components/Form'
 import { H1 } from '../../components/H'
 import { Link } from '../../components/Link'
 import { SEO } from '../../components/SEO'
-import { authRegister } from '../../functions/auth'
-import { getFormData } from '../../functions/getFormData'
-import { makePostRequest } from '../../functions/makeRequest'
+import { authRegister } from '../../functions/api'
 
 const Members: NextPage = () => {
     const router = useRouter()
 
-    return(
+    return (
         <>
             <SEO
                 title='L2 Planet | Members'
@@ -27,32 +20,29 @@ const Members: NextPage = () => {
             />
 
             <H1>Register</H1>
-            
-            <Div>
-                <LoginForm onSubmit={async (data) => {
-                    
 
-                    const res = await authRegister(data)
-                    console.log(res)
-                    if(res === null) {
-                        alert('An error occured.')
-                    } else {
+            <Div>
+                <LoginForm onSubmit={async (formData) => {
+                    try {
+                        await authRegister(formData)
                         alert('Succesfully registered!')
-                        setTimeout(() => router.push('/members/login'), 2000)
+                        alert(`Let's log in!`)
+                        router.push('/members/login')
+                    } catch (e: any) {
+                        alert(e.message)
                     }
-                    
-                }}/>
+                }} />
 
                 <Link a={A} href='/members/login'>Already have an account? Login.</Link>
             </Div>
 
-            
+
         </>
     )
 }
-            
+
 export default Members
-            
+
 
 const Div = wrapn('div')`
     flex
