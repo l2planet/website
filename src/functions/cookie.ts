972343b395e5bf7ecfb9361ec4b3b24b187589d1
Berzan <berzan@tutanota.com>
2022-09-29 10:36:44 +0300

    update: cookie.ts

--- src/functions/cookie.ts
+++ src/functions/cookie.ts
@@ -1,18 +1,24 @@
-export function setCookie(name: string, val: string, expires: string) {
-    document.cookie = `${name}=${val}; expires=${expires}; path=/`
+/** Sets the jwt cookie. */
+export function setJwtCookie(token: string, expires: string) {
+    document.cookie = `jwt=${token}; expires=${expires}; path=/`
 }
 
-export function getCookie(name: string) {
-    const key = name + "=";
-    const cookies = document.cookie.split(";");
+/** Returns the jwt cookie. If there is no cookie, throws an error. */
+export function getJwtCookie(): string {
+    const key = "jwt="
+
+    const cookies = document.cookie.split(";")
+
     for (let i = 0; i < cookies.length; i++) {
-        let cookie = cookies[i];
+        let cookie = cookies[i]
+
         while (cookie.charAt(0) === " ") {
             cookie = cookie.substring(1, cookie.length);
         }
+
         if (cookie.indexOf(key) === 0) {
             return cookie.substring(key.length, cookie.length);
         }
     }
-    return null;
+    throw new Error('You must be logged in!')
 }
