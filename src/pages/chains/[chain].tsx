52ad43e65d793e31c5b0e17e5151069e48bf95e7
Berzan <berzan@tutanota.com>
2022-09-30 20:40:15 +0300

    remove: content for testing

--- src/pages/chains/[chain].tsx
+++ src/pages/chains/[chain].tsx
@@ -16,62 +16,15 @@ const SPARE_ICON =
     'https://raw.githubusercontent.com/l2planet/images/6c2f47bea857e600a66ce688b9e94887cd7ba41a/chains/ethereum.svg'
 
 const Chain: NextPage = () => {
-    const { useChain } = useApi()
-    const chain = useChain()
-    const [category, setCategory] = useState('')
 
     return (
         <>
             <SEO
-                title={`L2 Planet | ${chain?.name || 'Chain'}`}
+                title={`L2 Planet | ${'Chain'}`}
                 description='L2 Planet'
                 favicon='/favicon.ico'
             />
 
-            <SectionChainIntro>
-                <Img
-                    src={chain?.icon || SPARE_ICON}
-                    img={ImgChain}
-                    alt={`${chain?.name || 'Chain'} Logo`}
-                />
-                <DivMeta>
-                    <HChain>{chain?.name || 'Chain'}</HChain>
-                    <PChainDescription>
-                        {chain?.description ||
-                            'Lorem ipsum dolor sit amet, et lorem ipsum dolor sit amet.'}
-                    </PChainDescription>
-                </DivMeta>
-            </SectionChainIntro>
-            <SectionChainL2s>
-                <Col4Select>
-                    <HSelect>Layer 2 Category:</HSelect>
-                    <Select
-                        onSelect={(category) => setCategory(category)}
-                        options={chain?.l2Categories || []}
-                    />
-                </Col4Select>
-                <Grid4ChainPage>
-                    {chain?.layer2s.at(0) ? (
-                        <>
-                            {category
-                                ? chain?.layer2s
-                                      .filter((l2) =>
-                                          l2.categories.includes(category)
-                                      )
-                                      .map((l2) => (
-                                          <CardLayer2 key={l2.id} {...l2} />
-                                      ))
-                                : chain?.layer2s.map((l2) => (
-                                      <CardLayer2 key={l2.id} {...l2} />
-                                  ))}
-                        </>
-                    ) : (
-                        <Text>
-                            We can't find any layer 2 for {chain?.name}. 🙁
-                        </Text>
-                    )}
-                </Grid4ChainPage>
-            </SectionChainL2s>
         </>
     )
 }
