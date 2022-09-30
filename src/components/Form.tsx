b84427ab33d0ab9d0236ce2ba66da464a05e50a1
Berzan <berzan@tutanota.com>
2022-09-30 12:58:48 +0300

    update: FormLayer2

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import { wrapn } from "wrapn";
 import { getFormData } from "../functions/getFormData";
-import { RawFormChain, RawFormLayer2, RawFormAuth, RawFormProject } from "../types/Api";
+import { RawFormChain, RawFormLayer2, RawFormAuth, RawFormProject, RawBridge } from "../types/Api";
 import { FormProps, LabeledInputProps } from "../types/globals";
+import { Bridger } from "./Bridger";
 import { ButtonForm } from "./Button";
 
 const Form = wrapn('form')`
@@ -46,6 +47,7 @@ const Input = wrapn('input')`
     outline-none
 
     font-semibold
+    placeholder:font-medium
 
     h-10
 
@@ -68,6 +70,7 @@ const TextArea = wrapn('textarea')`
     outline-none
 
     font-semibold
+    placeholder:font-medium
 
     h-40
 
@@ -98,7 +101,7 @@ const LabeledInput = (props: LabeledInputProps) => {
     return <DivLabeledInput>
         <Label>{props.label}</Label>
         <ToolTipText>{props.tip}</ToolTipText>
-        <Input name={props.name} type={props.isPassword ? 'password' : 'text'}/>
+        <Input placeholder={props.placeHolder} name={props.name} type={props.isPassword ? 'password' : 'text'}/>
     </DivLabeledInput>
 }
 
@@ -109,7 +112,7 @@ const LabeledTextArea = (props: LabeledInputProps) => {
         <Label>{props.label}</Label>
         <ToolTipText>{props.tip}</ToolTipText>
         <input readOnly hidden name={props.name} type='text' value={val}/>
-        <TextArea onChange={(e) => setVal(e.target.value)}/>
+        <TextArea placeholder={props.placeHolder} onChange={(e) => setVal(e.target.value)}/>
     </DivLabeledInput>
 }
 
@@ -123,8 +126,8 @@ export const LoginForm = ({ onSubmit }: FormProps<RawFormAuth>) => {
             onSubmit(getFormData(e))
         }} className='self-center w-full max-w-2xl'>
             <DivForm>
-                <LabeledInput name='username' label='User Name' tip='Your user name.'/>
-                <LabeledInput name='password' label='Password' tip='Your password.' isPassword/>
+                <LabeledInput name='username' label='User Name' tip='Your user name.' placeHolder='elon'/>
+                <LabeledInput name='password' label='Password' tip='Your password.' placeHolder='••••••••' isPassword/>
             </DivForm>
             <ButtonForm type='submit'>Login</ButtonForm>
         </Form>
@@ -142,8 +145,8 @@ export const RegisterForm = ({ onSubmit }: FormProps<RawFormAuth>) => {
             onSubmit(getFormData(e))
         }} className='self-center w-full max-w-2xl'>
             <DivForm>
-                <LabeledInput name='username' label='User Name' tip='Your user name.'/>
-                <LabeledInput name='password' label='Password' tip='Your password.' isPassword/>
+                <LabeledInput name='username' label='User Name' tip='Your user name.' placeHolder='elon'/>
+                <LabeledInput name='password' label='Password' tip='Your password.' placeHolder='••••••••' isPassword/>
             </DivForm>
             <ButtonForm type='submit'>Register</ButtonForm>
         </Form>
@@ -164,9 +167,9 @@ export const ChainForm = ({ onSubmit }: FormProps<RawFormChain>) => {
             onSubmit(getFormData(e))
         }} className='self-center w-full max-w-3xl'>
             <DivForm>
-                <LabeledInput name='name' label='Chain Name' tip='The name of the chain.'/>
-                <LabeledInput name='icon' label='Icon URL' tip='URL of the chain icon as SVG.'/>
-                <LabeledTextArea name='description' label='Description' tip='A short description about the chain.'/>
+                <LabeledInput name='name' label='Chain Name' tip='The name of the chain.' placeHolder='Ethereum'/>
+                <LabeledInput name='icon' label='Icon URL' tip='URL of the chain icon as SVG.' placeHolder='https://raw.githubusercontent.com/l2planet/images/main/chains/ethereum.svg' />
+                <LabeledTextArea name='description' label='Description' tip='A short description about the chain.' placeHolder='Ethereum is...'/>
             </DivForm>
             <ButtonForm type='submit'>Add a New Chain</ButtonForm>
         </Form>
@@ -178,26 +181,31 @@ export const ChainForm = ({ onSubmit }: FormProps<RawFormChain>) => {
 
 
 
+
 /** Layer2 Form */
 export const Layer2Form = ({ onSubmit }: FormProps<RawFormLayer2>) => {
+    const [bridges, setBridges] = useState<RawBridge[]>([{
+        address: '',
+        tokens: '',
+    }])
+    
     return (
-        <Form  onSubmit={(e) => {
+        <Form onSubmit={(e) => {
             e.preventDefault()
-            onSubmit(getFormData(e))
+            onSubmit({...getFormData(e), bridges})
         }}>
             <DivForm>
-                <LabeledInput name='name' label='Layer 2 Name' tip='The name of the layer 2.'/>
-                <LabeledInput name='icon' label='Icon URL' tip='URL of the layer 2 icon as SVG.'/>
-                <LabeledTextArea name='description' label='Description' tip='A long description about the layer 2.'/>
-                <LabeledInput name='categories' label='Categories' tip='For example: SNARKs, STARKs, ZK, etc. (COMMA SEPERATED)'/>
-                <LabeledInput name='website' label='Website URL' tip='URL of the official website.'/>
-                <LabeledInput name='evmId' label='EVM ID' tip='EVM ID of the layer 2.'/>
-                <LabeledInput name='bridges' label='Bridge Contracts' tip='Contract adresses of the bridges. (COMMA SEPERATED)'/>
-                <LabeledInput name='tokens' label='Token Tickers' tip='Tickers of the bridge supported assets. (COMMA SEPERATED)'/>
-                <LabeledInput name='twitter' label='Twitter URL' tip='URL of the official Twitter account. (OPTIONAL)'/>
-                <LabeledInput name='videos' label='Video URLs' tip='URLs of YouTube videos. (COMMA SEPERATED)'/>
-                <LabeledInput name='gecko' label='Coin Gecko URL' tip='Coin Gecko URL of the native coin of the layer 2. (OPTIONAL)'/>
-                <LabeledInput name='investors' label='Investor Icons' tip='URL of the icons of the investors. (COMMA SEPERATED)'/>
+                <LabeledInput name='name' label='Layer 2 Name' tip='The name of the layer 2.' placeHolder='Arbitrum'/>
+                <LabeledInput name='icon' label='Icon URL' tip='URL of the layer 2 icon as SVG.' placeHolder='https://example.com/atbitrum.svg'/>
+                <LabeledTextArea name='description' label='Description' tip='A long description about the layer 2.' placeHolder='Arbitrum is...' />
+                <LabeledInput name='categories' label='Categories' tip='For example: SNARKs, STARKs, ZK, etc. (COMMA SEPERATED)' placeHolder='SNARKs, ZK'/>
+                <LabeledInput name='website' label='Website URL' tip='URL of the official website.' placeHolder='https://arbitrum.io'/>
+                <LabeledInput name='evmId' label='EVM ID' tip='EVM ID of the layer 2.' placeHolder='35'/>
+                <Bridger bridges={bridges} setBridges={setBridges}/>
+                <LabeledInput name='twitter' label='Twitter URL' tip='URL of the official Twitter account. (OPTIONAL)' placeHolder='https://twitter.com/arbitrum'/>
+                <LabeledInput name='videos' label='Video URLs' tip='URLs of YouTube videos. (COMMA SEPERATED)' placeHolder='https://youtu.be/ufKxCczY7-c , https://youtu.be/usDxRtl35-c'/>
+                <LabeledInput name='gecko' label='Coin Gecko URL' tip='Coin Gecko URL of the native coin of the layer 2. (OPTIONAL)' placeHolder='https://www.coingecko.com/en/coins/arbitrum'/>
+                <LabeledInput name='investors' label='Investor Icons' tip='URL of the icons of the investors. (COMMA SEPERATED)' placeHolder='https://example.com/venture-capital-1.svg , https://example.com/venture-capital-2.svg'/>
             </DivForm>
             <ButtonForm type='submit'>Add a New Layer 2</ButtonForm>
         </Form>
@@ -217,12 +225,12 @@ export const ProjectForm = ({ onSubmit }: FormProps<RawFormProject>) => {
             onSubmit(getFormData(e))
         }}>
             <DivForm>
-                <LabeledInput name='name' label='Project Name' tip='The name of the project.'/>
-                <LabeledInput name='icon' label='Icon URL' tip='URL of the project icon as SVG.'/>
-                <LabeledTextArea name='description' label='Description' tip='Description about the project.'/>
-                <LabeledInput name='categories' label='Categories' tip='For example: DEX, NFT Marketplace, AMM, etc. (COMMA SEPERATED)'/>
-                <LabeledInput name='website' label='Website' tip='URL of the official website. (OPTIONAL)'/>
-                <LabeledInput name='twitter' label='Twitter URL' tip='URL of the official Twitter account. (OPTIONAL)'/>
+                <LabeledInput name='name' label='Project Name' tip='The name of the project.' placeHolder='Uniswap'/>
+                <LabeledInput name='icon' label='Icon URL' tip='URL of the project icon as SVG.' placeHolder='https://example.com/uniswap.svg'/>
+                <LabeledTextArea name='description' label='Description' tip='Description about the project.' placeHolder='Uniswap is...'/>
+                <LabeledInput name='categories' label='Categories' tip='For example: DEX, NFT Marketplace, AMM, etc. (COMMA SEPERATED)' placeHolder='DEX, AMM'/>
+                <LabeledInput name='website' label='Website' tip='URL of the official website. (OPTIONAL)' placeHolder='https://uniswap.org'/>
+                <LabeledInput name='twitter' label='Twitter URL' tip='URL of the official Twitter account. (OPTIONAL)' placeHolder='https://twitter.com/uniswap'/>
             </DivForm>
             <ButtonForm type='submit'>Add a New Project</ButtonForm>
         </Form>
