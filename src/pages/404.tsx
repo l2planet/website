410ec172ac61d72280af54688abc3d39ed1865be
Berzan <berzan@tutanota.com>
2022-10-01 10:02:09 +0300

    add: custom 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,24 @@
+import { wrapn } from "wrapn"
+import { H1 } from "../components/H"
+import { Link } from "../components/Link"
+
+function Page404() {
+    return (
+        <>
+            <H1>404 Not Found</H1>
+            <Link a={A} href='/'>Go back home</Link>
+        </>
+    )
+}
+
+export default Page404
+
+const A = wrapn('a')`
+    text-center
+
+    font-semibold
+    text-xl md:text-2xl
+
+    text-gris-8
+    dark:text-gris-2
+`
