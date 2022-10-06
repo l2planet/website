73af6c685e43d644340ab3fb8c39b6150c7154b1
Berzan <berzan@tutanota.com>
2022-10-06 20:53:49 +0300

    update: content

--- src/pages/stats.tsx
+++ src/pages/stats.tsx
@@ -2,9 +2,15 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { Flex4StatsTables } from '../components/Div'
 import { SEO } from '../components/SEO'
+import { TableTPS, TableTVL } from '../components/Table'
+import { useApi } from '../contexts/ApiContext'
 
 const Stats: NextPage = () => {
+    const { useTVLs } = useApi()
+    const tvls = useTVLs()
+
     return (
         <>
             <SEO
@@ -12,6 +18,13 @@ const Stats: NextPage = () => {
                 description='L2 Planet'
                 favicon='/favicon.ico'
             />
+
+            {tvls && (
+                <Flex4StatsTables>
+                    <TableTVL data={tvls} />
+                    <TableTPS data={[]} />
+                </Flex4StatsTables>
+            )}
         </>
     )
 }
