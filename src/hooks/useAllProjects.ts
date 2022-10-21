9d8aaa3912ed4efdd855de32b31b58c2cc1721f1
Berzan <berzan@tutanota.com>
2022-10-21 17:02:19 +0300

    create: useAllProjects hook

---
+++ src/hooks/useAllProjects.ts
@@ -0,0 +1,37 @@
+import { useMemo } from 'react'
+import { useInfoEndpoint } from '../contexts/InfoEndpointContext'
+import { APIGetChain, APIGetProject, InternalChain, InternalProject } from '../types/Api'
+
+/**
+ * The hook that enables getting all the projects available on the backend inside a component.
+ *
+ * # Usage
+ * ```tsx
+ * export const Comp = () => {
+ *     const { projects } = useAllProjects()
+ *     return </>
+ * }
+ * ```
+ */
+export function useAllProjects(): AllProjects {
+    // Extract `apiManager`.
+    const { endpointInfo } = useInfoEndpoint()
+
+    // Declare `chains` memoized value.
+    const projects: InternalProject[] | undefined = useMemo(() => {
+        if (!endpointInfo) return undefined
+
+        return Object.entries(endpointInfo.projects).map(([id, data]) => ({
+            ...(data as APIGetProject),
+            id,
+        }))
+    }, [endpointInfo])
+
+    // Return `chains` inside a readonly object.
+    return { projects } as const
+}
+
+/** The interface for the return value of `useAllProjects`. */
+interface AllProjects {
+    projects?: InternalProject[]
+}
