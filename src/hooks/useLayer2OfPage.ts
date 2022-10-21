8ffb0e876e656c60f00a6351bc7b22a899289bbf
Berzan <berzan@tutanota.com>
2022-10-21 15:29:51 +0300

    create: useLayer2OfPage hook

---
+++ src/hooks/useLayer2OfPage.ts
@@ -0,0 +1,54 @@
+import { useMemo } from "react";
+import { useApi } from "../contexts/ApiContext";
+import { useInfoEndpoint } from "../contexts/InfoEndpointContext";
+import { APIGetLayer2, InternalChain, InternalLayer2 } from "../types/Api";
+import { useRoute } from "./useRoute";
+
+/**
+ * The hook that enables getting the layer 2 solution with the ID of `id` URL parameter.
+ * 
+ * # Usage
+ * ```tsx
+ * export const Comp = () => {
+ *     const { layer2 } = useLayer2OfPage()   
+ *     return </>
+ * }
+ * ```
+ */
+export function useLayer2OfPage(): Layer2OfPage {
+    // Extract `apiManager`.
+    const { endpointInfo } = useInfoEndpoint()
+    const { id, navigateToNotFound } = useRoute()
+
+
+    // Declare `layer2` memoized value.
+    const layer2: InternalLayer2 | undefined = useMemo(() => {
+        if (!endpointInfo) return
+        if (!id) {
+            navigateToNotFound()
+            return
+        }
+
+        const _layer2 = endpointInfo.layer2s[id]
+
+        if (!_layer2) {
+            navigateToNotFound()
+            return
+        }
+
+        return {
+            ..._layer2,
+            id,
+        } as unknown as InternalLayer2
+
+
+    }, [endpointInfo, id, navigateToNotFound])
+
+    // Return `chain` inside a readonly object.
+    return { layer2 } as const
+}
+
+/** The interface for the return value of `useLayer2OfPage`. */
+interface Layer2OfPage {
+    layer2?: InternalLayer2
+}
