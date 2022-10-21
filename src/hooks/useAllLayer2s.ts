a93dc42b03ff4d5b79b77abc4241a4e97e53ad26
Berzan <berzan@tutanota.com>
2022-10-21 19:01:54 +0300

    create: useAllLayer2s hook

---
+++ src/hooks/useAllLayer2s.ts
@@ -0,0 +1,37 @@
+import { useMemo } from 'react'
+import { useInfoEndpoint } from '../contexts/InfoEndpointContext'
+import { APIGetLayer2, InternalLayer2 } from '../types/Api'
+
+/**
+ * The hook that enables getting all the chains available on the backend inside a component.
+ *
+ * # Usage
+ * ```tsx
+ * export const Comp = () => {
+ *     const { layer2s } = useAllLayer2s()
+ *     return </>
+ * }
+ * ```
+ */
+export function useAllLayer2s(): AllLayer2s {
+    // Extract `apiManager`.
+    const { endpointInfo } = useInfoEndpoint()
+
+    // Declare `layer2s` memoized value.
+    const layer2s: InternalLayer2[] | undefined = useMemo(() => {
+        if (!endpointInfo) return undefined
+
+        return Object.entries(endpointInfo.layer2s).map(([id, data]) => ({
+            ...(data as APIGetLayer2),
+            id,
+        })) as unknown as InternalLayer2[]
+    }, [endpointInfo])
+
+    // Return `layer2s` inside a readonly object.
+    return { layer2s } as const
+}
+
+/** The interface for the return value of `useAllLayer2s`. */
+interface AllLayer2s {
+    layer2s?: InternalLayer2[]
+}
