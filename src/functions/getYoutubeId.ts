0eca3a159a132696c24296fbfa7e40468623c0f7
Berzan <berzan@tutanota.com>
2022-10-23 14:03:15 +0300

    update: getYoutubeId

--- src/functions/getYoutubeId.ts
+++ src/functions/getYoutubeId.ts
@@ -1,26 +1,19 @@
 /** Extracts Youtube ID from the URL. */
-export function getYoutubeId(url: string): null | string {
-    url = url.trim()
+export function getYoutubeId(youtubeUrl: string): null | string {
+    youtubeUrl = youtubeUrl.trim()
 
-    if (url.startsWith('https://')) {
-        url = url.slice(8)
-    }
-
-    if (url.startsWith('youtu.be/')) {
-        url = url.slice(9)
-    }
-
-    if (url.startsWith('www.youtube.com/')) {
-        url = url.slice(16)
-    }
-
-    if (url.startsWith('watch?v=')) {
-        url = url.slice(8)
-    }
-
-    if (url.length >= 11) {
-        return url.slice(0, 11)
-    } else {
+    try {
+        const url = new URL(youtubeUrl)
+        if (url.host == 'www.youtube.com') {
+            const id = url.searchParams.get('v')
+            return id !== null && id.length === 11 ? id : null
+        } else if (url.host == 'youtu.be') {
+            const id = url.pathname.slice(1)
+            return id.length === 11 ? id : null
+        } else {
+            return null
+        }
+    } catch {
         return null
     }
 }
