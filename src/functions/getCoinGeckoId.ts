fb80e827697f3810cd504b6191d04937576ea5af
Berzan <berzan@tutanota.com>
2022-10-23 14:02:53 +0300

    update: getCoinGeckoID

--- src/functions/getCoinGeckoId.ts
+++ src/functions/getCoinGeckoId.ts
@@ -1,24 +1,21 @@
 /** Extracts Coingecko ID from the URL. */
-export function getCoinGeckoId(url: string): null | string {
-    url = url.trim()
+export function getCoinGeckoId(geckoUrl: string): null | string {
+    geckoUrl = geckoUrl.trim()
 
-    if (url.startsWith('https://')) {
-        url = url.slice(8)
+    try {
+        const url = new URL(geckoUrl)
+        if (url.host == 'coingecko.com') {
+            const [coinsPath, coinId] = url.pathname.split('/').slice(1, 3)
+
+            if (coinsPath === 'coins' && coinId.length > 0) {
+                return coinId
+            } else {
+                return null
+            }
+        } else {
+            return null
+        }
+    } catch {
+        return null
     }
-
-    if (url.startsWith('www.')) {
-        url = url.slice(4)
-    }
-
-    if (url.startsWith('coingecko.com/')) {
-        url = url.slice(14)
-    }
-
-    let [path, name] = url.split('/').slice(1, 3)
-
-    if (path === 'coins' && name.length > 0) {
-        return name
-    }
-
-    return null
 }
