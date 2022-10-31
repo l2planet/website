b7eb80d2404f2b983b36be6e2c066d6e7affa741
Berzan <berzan@tutanota.com>
2022-10-31 04:10:31 -0700

    add: null control

--- src/hooks/useRawProjectOfPage.ts
+++ src/hooks/useRawProjectOfPage.ts
@@ -27,27 +27,28 @@ export function useRawProjectOfPage(): ChainOfPage {
             return
         }
 
-        const _projects = endpointInfo.projects[id]
+        const _project = endpointInfo.projects[id]
 
-        if (_projects === undefined) {
+        if (_project === undefined) {
             navigateToNotFound()
             return
         }
 
         const layer2_ids: string[] = []
+        1
 
         for (const [layer2Id, layer2Data] of Object.entries(endpointInfo.layer2s) as [
             string,
             APIGetLayer2
         ][]) {
-            if (layer2Data.projects.includes(id)) {
+            if (layer2Data.projects !== null && layer2Data.projects.includes(id)) {
                 layer2_ids.push(layer2Id)
             }
         }
 
         return {
-            ..._projects,
-            categories: _projects.categories ?? [],
+            ..._project,
+            categories: _project.categories ?? [],
             id,
             layer2_ids,
         }
