0b6767407e342d9318ca32fa5e35433223f6f637
Berzan <berzan@tutanota.com>
2022-11-05 13:56:12 +0300

    change: icon

--- src/components/icons/IconStats.tsx
+++ src/components/icons/IconStats.tsx
@@ -6,6 +6,6 @@ export const IconStats = () => (
         height='100%'
         width='100%'
     >
-        <path d='M8 40V18h7v22Zm12.5 0V8h7v32ZM33 40V26h7v14Z' />
+        <path d="M14 40v-4.5h-4v-23h4V8h3v4.5h4v23h-4V40Zm-1-7.5h5v-17h-5ZM31 40V29.5h-4v-13h4V8h3v8.5h4v13h-4V40Zm-1-13.5h5v-7h-5ZM15.5 24Zm17-1Z" />
     </svg>
 )
