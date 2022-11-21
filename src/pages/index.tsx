8eda57a89adadc3f3b23be68110e0e3890412b83
Berzan <berzan@tutanota.com>
2022-11-21 22:03:53 +0300

    add: partners section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,11 @@ import { CardIndex } from '../components/Card'
 import { H1, H2 } from '../components/H'
 import { SubscribeForm } from '../components/SubscribeForm'
 import { P1 } from '../components/P'
-import { SectionIndexCards, SectionIndexIntro, SectionIndexSubscribe } from '../components/Section'
+import { SectionIndexCards, SectionIndexIntro, SectionIndexSubscribe, SectionPartners } from '../components/Section'
 import { SEO } from '../components/SEO'
 import { useAllChains } from '../hooks/useAllChains'
+import { wrapn } from 'wrapn'
+import { Partners } from '../components/Partners'
 
 const Home: NextPage = () => {
     const { chains } = useAllChains()
@@ -38,8 +40,15 @@ const Home: NextPage = () => {
                 </P1>
                 <SubscribeForm />
             </SectionIndexSubscribe>
+
+            <SectionPartners>
+                <H2>Partners</H2>
+                <Partners />
+            </SectionPartners>
+
         </>
     )
 }
 
 export default Home
+
