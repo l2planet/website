fe3697f93cf3e6b541979406cd1637d5702620b4
Berzan <berzan@tutanota.com>
2022-12-04 09:53:44 +0300

    add: Seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,21 +6,36 @@ import { TopBar } from '../components/TopBar'
 import { Main } from '../components/Main'
 import { Footer } from '../components/Footer'
 import { InfoEndpointProvider } from '../contexts/InfoEndpointContext'
-import { NextSeo } from 'next-seo'
+import { DefaultSeo, NextSeo } from 'next-seo'
 
 // This component is shared by every page.
 // The data on it is stored across different pages.
 function MyApp({ Component, pageProps }: AppProps) {
     return (
-        <ThemeProvider>
-            <InfoEndpointProvider>
-                <TopBar />
-                <Main>
-                    <Component {...pageProps} />
-                </Main>
-                <Footer />
-            </InfoEndpointProvider>
-        </ThemeProvider>
+        <>
+            <DefaultSeo title='L2 Planet | Home'
+                description='L2 Planet is a project which works for public goods. The main goal is to accelerate Layer-2 adaptation and educate the community about it.'
+                twitter={{
+                    cardType: 'summary_large_image',
+                    handle: 'twitter:layer2planet',
+                    site: 'twitter:layer2planet',
+
+                }}
+                openGraph={{
+                    type: 'website',
+                    locale: 'en_US',
+                    siteName: 'Layer 2 Planet'
+                }} />
+            <ThemeProvider>
+                <InfoEndpointProvider>
+                    <TopBar />
+                    <Main>
+                        <Component {...pageProps} />
+                    </Main>
+                    <Footer />
+                </InfoEndpointProvider>
+            </ThemeProvider>
+        </>
     )
 }
 
